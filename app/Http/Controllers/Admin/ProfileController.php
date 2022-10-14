f9cb9a87203a050aa27f3f4fcb2e02923162f047
m.shimamura <m.shimamura@ae.auone-net.jp>
2022-10-14 12:01:04 +0000

    【PHP/Laravel】09課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     //ここから追記
@@ -13,10 +15,29 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    // public function create()
+    // {
+    //     return redirect('admin/profile/create');
+    // }
+    
+    public function create(Request $request)
     {
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $news = new Profile;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
+        // admin/profile/createにリダイレクトする
         return redirect('admin/profile/create');
-    }
+    } 
 
     public function edit()
     {
